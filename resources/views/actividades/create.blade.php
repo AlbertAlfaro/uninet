755f78e64f1afddc76c8b6299e74314e40b7ecc9
Durjan <durjan.alvarado94@gmail.com>
2021-06-02 21:55:16 -0600

    se borro funciones que no se ocupaban en el archivo

--- resources/views/actividades/create.blade.php
+++ resources/views/actividades/create.blade.php
@@ -79,26 +79,8 @@ Actividades
     <script src="{{ URL::asset('assets/libs/parsleyjs/parsleyjs-spanish.js')}}"></script>
 
     <script type="text/javascript">
-    $('#id_region').on('change', function() {
-        var id = $("#id_region").val();
-        filtro(id);
-    });
-    function filtro(id) {
-        // Guardamos el select de cursos
-        var lab = $("#id_lab");
 
-        $.ajax({
-            type:'GET',
-            url:'{{ url("lab/filtro") }}/'+id,
-            success:function(data) {
-                lab.find('option').remove();
-                lab.append('<option value="">Seleccionar...</option>');
-                $(data).each(function(i, v){ // indice, valor
-                    lab.append('<option value="' + v.id_laboratorio + '">' + v.nombre_lab + '</option>');
-                })
-            }
-        });
-    }
+ 
         $(function () {
           $('#form').parsley().on('field:validated', function() {
             var ok = $('.parsley-error').length === 0;
@@ -108,107 +90,6 @@ Actividades
         
         });
 
-        $( "#nombres" ).change(function() {
-            var nombre = $("#nombres").val();
-            var apellido = $("#apellidos").val();
-            $("#img-perfil").attr("src","https://ui-avatars.com/api/?name="+nombre+"+"+apellido+"&size=128");
-
-        });
-        $( "#apellidos" ).change(function() {
-            var nombre = $("#nombres").val();
-            var apellido = $("#apellidos").val();
-            $("#img-perfil").attr("src","https://ui-avatars.com/api/?name="+nombre+"+"+apellido+"&size=128");
-
-        });
-
-        $( "#password" ).change(function() {
-            var password = $("#password").val();
-            var password2 = $("#password2").val();
-            if(password!="" && password2!=""){
-                if(password != password2){
-                    $("#error").show();
-                    $("#guardar").prop('disabled', true);
-                }else{
-                    $("#error").hide();
-                    $("#guardar").prop('disabled', false);
-
-                }
-            }
-            if(password=="" && password2==""){
-                $("#guardar").prop('disabled', false);
-                $("#error").hide();
-
-            }
-            
-
-        });
-        $( "#password2" ).change(function() {
-            var password = $("#password").val();
-            var password2 = $("#password2").val();
-
-            if(password!="" && password2!=""){
-                if(password != password2){
-                    $("#error").show();
-                    $("#guardar").prop('disabled', true);
-                }else{
-                    $("#error").hide();
-                    $("#guardar").prop('disabled', false);
-
-                }
-            }
-            if(password=="" && password2==""){
-                $("#guardar").prop('disabled', false);
-                $("#error").hide();
-
-            }
-            
-
-        });
-
-        $( "#email" ).change(function() {
-            verificacion_email($("#email").val());
-        });
-        $( "#nombre" ).change(function() {
-            verificacion_user($("#nombre").val());
-        });
-
-        function verificacion_email(email){
-            $.ajax({
-                type: 'GET',
-                url: 'verificacion_email/'+email+'/0',
-                
-                success: function(data) {
-                    if(data.mensaje==1){
-                        $("#error_email").show();
-                        $("#guardar").prop('disabled', true);
-
-                    }else{
-                        $("#error_email").hide();
-                        $("#guardar").prop('disabled', false);
-
-                    }
-                }
-            });
-        }
-
-        function verificacion_user(user){
-            $.ajax({
-                type: 'GET',
-                url: 'verificacion_user/'+user+'/0',
-                
-                success: function(data) {
-                    if(data.mensaje==1){
-                        $("#error_username").show();
-                        $("#guardar").prop('disabled', true);
-
-                    }else{
-                        $("#error_username").hide();
-                        $("#guardar").prop('disabled', false);
-
-                    }
-                }
-            });
-        }
     </script>
 
     

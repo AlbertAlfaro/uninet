aea57a558097dcd560b70e10b5818d968aa4b812
Durjan <durjan.alvarado94@gmail.com>
2021-08-09 00:07:09 -0600

    facturacion

--- app/Http/Controllers/FacturacionController.php
+++ app/Http/Controllers/FacturacionController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 use App\Models\Cobrador;
+use App\Models\Correlativo;
 use App\Models\Cliente;
 use App\Models\Internet;
 use App\Models\Tv;
@@ -50,7 +51,7 @@ class FacturacionController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        return "hello";
     }
 
     /**
@@ -105,6 +106,7 @@ class FacturacionController extends Controller
         $queries = Cliente::
         Where('codigo', 'LIKE', '%'.$term1.'%')->
         orWhere('nombre', 'LIKE', '%'.$term1.'%')->
+        Where('id_sucursal',Auth::user()->id_sucursal)->
         get();    
         foreach ($queries as $query){
             $results[] = [ 'id' => $query->id, 'value' => "(".$query->codigo.") ".$query->nombre,'nombre' => $query->nombre,'tipo_documento'=>$query->tipo_documento,'direc'=>$query->dirreccion,'nit'=>$query->nit];
@@ -128,10 +130,17 @@ class FacturacionController extends Controller
     
             }
             if($servi)
-            {  $abono = Abono::where('id_cliente',$id_cliente)->where('abono','0.00')->get();
-               foreach ($abono as $query){
-                  $results[] = [ 'id' => $query->id, 'cargo' => $query->cargo,'mes_servicio' => $query->mes_servicio->format('m/Y'),'fecha_vence'=>$query->fecha_vence->format('d/m/Y')];
-              }
+            {   $abono = Abono::where('id_cliente',$id_cliente)->where('abono','0.00')->where('pagado','0')->get();
+                if($abono)
+                {
+                    foreach ($abono as $query){
+                        $results[] = [ 'id' => $query->id, 'cargo' => $query->cargo,'mes_servicio' => $query->mes_servicio->format('m/Y'),'fecha_vence'=>$query->fecha_vence->format('d/m/Y'),'mes_ser'=>$query->mes_servicio->format('Y/m/d')];
+                    }
+                }else
+                {
+                    $results=[];
+                }
+           
               return response($results);   
                /*$abono = Abono::where('id_cliente',$id_cliente)->where('abono','0.00')->get();
                 return response()->json(
@@ -157,5 +166,109 @@ class FacturacionController extends Controller
           'letras' => $letras,
       ]);
     }
+    public function guardar(Request $request)
+    {
+        if ($request->cuantos >0)
+        {
+            $array = json_decode($request->json_arr, true);
+            if($request->tipo_impresion==1){$tipo="FAC";}
+            if($request->tipo_impresion==2){$tipo="CRE";}
+            foreach ($array as $fila)
+            {   $abono = new Abono();
+                $abono->id_cliente=$request->id_cliente;
+                $abono->id_cobrador=$request->id_cobrador;
+                $abono->id_usuario=Auth::user()->id;
+                $abono->recibo = $request->numreci;
+                $abono->tipo_servicio=$request->tipo_servicio;
+                $abono->numero_documento=$tipo."-".$request->numdoc;
+                $abono->tipo_documento=$request->tipo_impresion;
+                $abono->tipo_pago=$request->tipo_pago;
+                $abono->mes_servicio=$fila['mes_ser'];
+                $abono->fecha_aplicado=date('Y/m/d');
+                $abono->fecha_vence=Carbon::createFromFormat('d/m/Y', $fila['fecha_ven']);
+                $abono->cargo=0;
+                $abono->abono=$fila['precio'];
+                $abono->anulado=0;
+                $abono->pagado=0;
+                $abono->save();
+                if($abono)
+                {
+                    Abono::where('id',$fila['id'])->update(['pagado' =>'1']);
+                }
+                
+            }
+            Cobrador::where('id',$request->id_cobrador)->update(['recibo_ultimo' =>$request->numreci]);
+            $this->setCorrelativo($request->tipo_impresion);
+            return "Guradado con exito";
+            
+        }else{
+            return "No hay abonos para ingresar";
+        }
+    }
+
+    public function correlativo($id){// estaa como private
+
+
+        $correlativo = Correlativo::find($id);
+        $ultimo = $correlativo->ultimo+1;
+
+        return $this->get_correlativo($ultimo,6);
+
+    }
+
+    private function setCorrelativo($id){
+
+        //id correlativo 
+        /*
+            1 cof
+            2 ccf 
+            3 cliente
+            4 tv 
+            5 inter
+            6 orden 
+            7 traslado
+            8 reconexion
+            9 suspension
+        */
+        $correlativo = Correlativo::find($id);
+        $ultimo = $correlativo->ultimo+1;
+        Correlativo::where('id',$id)->update(['ultimo' =>$ultimo]);
+    }
+
+    private function get_correlativo($ult_doc,$long_num_fact){
+        $ult_doc=trim($ult_doc);
+        $len_ult_valor=strlen($ult_doc);
+        $long_increment=$long_num_fact-$len_ult_valor;
+        $valor_txt="";
+        if ($len_ult_valor<$long_num_fact) {
+            for ($j=0;$j<$long_increment;$j++) {
+            $valor_txt.="0";
+            }
+        } else {
+            $valor_txt="";
+        }
+        $valor_txt=$valor_txt.$ult_doc;
+        return $valor_txt;
+    }
+
+    public function num_recibo($id_cobrador)
+    {
     
+       $correlativo = Cobrador::find($id_cobrador);
+        $ultimo = $correlativo->recibo_ultimo+1;
+
+        return $this->get_correlativo($ultimo,5);
+    }
+    /*
+    public function ultimo_mes($id_cliente, $tipo_ser)
+    {
+        $abono= Abono::where('id_cliente',$id_cliente)->where('tipo_servicio',$tipo_ser)->get();
+        $abono1=$abono->last();
+        $mes_servicio=date("d-m-Y", strtotime($abono1->mes_servicio->format('d/m/Y')."+ 1 month"));
+        $fecha_vence=date("d-m-Y", strtotime($mes_servicio."+ 10 days"));
+        //$results[] = [ 'id' => $abono1->id, 'cargo' => $abono1->cargo,'mes_servicio' =>$mes_servicio->format('m/Y'),'fecha_vence'=>$fecha_vence,'mes_ser'=>$mes_servicio];
+        return response($abono1);
+        //nota: me traigo el precio de lo que pago por la ultima mensualidad
+    }*/
+
 }

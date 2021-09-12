ad164d8232d8c2b91fbd9a13c72054de327f2423
AlbertAlfaro <alber.cx@gmail.com>
2021-09-12 17:21:17 -0600

    Cambio a local

--- config/backup.php
+++ config/backup.php
@@ -117,7 +117,7 @@ return [
              */
             'disks' => [
                 'google',
-                'backup',
+                'local',
             ],
         ],
 
@@ -198,7 +198,7 @@ return [
     'monitor_backups' => [
         [
             'name' => env('APP_NAME', 'laravel-backup'),
-            'disks' => ['backup'],
+            'disks' => ['local'],
             'health_checks' => [
                 \Spatie\Backup\Tasks\Monitor\HealthChecks\MaximumAgeInDays::class => 1,
                 \Spatie\Backup\Tasks\Monitor\HealthChecks\MaximumStorageInMegabytes::class => 5000,

1c003b0d97cd7c9147cb8d7a8f4178e977e69432
Durjan <durjan.alvarado94@gmail.com>
2021-11-11 04:24:41 -0600

    cargar abonos

--- app/Imports/AbonosImport.php
+++ app/Imports/AbonosImport.php
@@ -3,7 +3,6 @@
 namespace App\Imports;
 use App\Models\Abono;
 use Carbon\Carbon;
-use DateTime;
 use Illuminate\Support\Collection;
 use Maatwebsite\Excel\Concerns\ToModel;
 
@@ -17,7 +16,7 @@ class AbonosImport implements ToModel
         return new Abono([
             'id_cliente' => $row[5],
             'numero_documento' => $row[0].$row[1],
-            'mes_servicio' => $row[2],
+            'mes_servicio' => \PhpOffice\PhpSpreadsheet\Shared\Date::excelToDateTimeObject($row[2]),
             'abono' => $row[3],
             'cesc_abono' => $row[4],
             'precio' => $row[3],

c0010376c4680a397fefddf87fd08289bbf17249
AlbertAlfaro <alber.cx@gmail.com>
2021-11-11 03:47:52 -0600

    Agregando correciones de clientes

--- app/Models/Internet.php
+++ app/Models/Internet.php
@@ -9,6 +9,8 @@ class Internet extends Model
 {
     use HasFactory;
     protected $dates = ['fecha_instalacion','contrato_vence','fecha_primer_fact'];
+    protected $fillable = ['id_cliente', 'numero_contrato','fecha_instalacion','fecha_primer_fact','cuota_mensual','dia_gene_fact','contrato_vence','periodo','velocidad','onu','onu_wifi','cable_red','router','identificador','activo'];
+
 
     public function get_cliente()
     {

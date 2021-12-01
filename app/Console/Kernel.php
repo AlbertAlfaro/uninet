7798f76cd1752ef3447e7360d967717033d6c7ad
Durjan <durjan.alvarado94@gmail.com>
2021-11-30 22:07:19 -0600

    se activo generar cobro

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -29,7 +29,7 @@ class Kernel extends ConsoleKernel
         // $schedule->command('inspire')->hourly();
 
         $schedule->command('backup:drive')->dailyAt('01:00');
-        //$schedule->command('cobros:genCobro')->dailyAt('02:00');
+        $schedule->command('cobros:genCobro')->dailyAt('02:00');
         //$schedule->command('cobros:genCobro')->everyMinute();
         
         //$schedule->command('backup:diario')->dailyAt('01:00');

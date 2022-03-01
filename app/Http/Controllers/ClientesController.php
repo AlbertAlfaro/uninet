b5377f71c10107a117252f9fba395a0c64ee14e6
Durjan <durjan.alvarado94@gmail.com>
2022-02-28 23:03:08 -0600

    gen cobro 30

--- app/Http/Controllers/ClientesController.php
+++ app/Http/Controllers/ClientesController.php
@@ -3167,7 +3167,7 @@ La suma antes mencionada la pagaré en esta ciudad, en las oficinas principales 
 
     public function gen_cobros(){
         //$dia_actual = date('j');
-        $dia_actual = 29;
+        $dia_actual = 30;
         $fecha_actual = date('Y-m-d');
         $fecha_vence = strtotime ( '+10 day' , strtotime ( $fecha_actual ) ) ;
         $fecha_vence = date ( 'Y-m-d' , $fecha_vence );
